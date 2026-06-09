52844041bf6ccfea2e00f584778e8370dfaac94e
vishnusajimon14-beep <vishnusajimon14@gmail.com>
2026-06-09 17:15:20 +0530

    uploaded chathist logic

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ from pinecone import Pinecone
 from langchain_openai import ChatOpenAI
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_pinecone import PineconeVectorStore
+from langchain.chains import create_history_aware_retriever
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage, AIMessage
 
-# load_dotenv()
+load_dotenv()
 
-PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
-PINECONE_INDEX_NAME = st.secrets["PINECONE_INDEX_NAME"]
-OPENROUTER_API_KEY=st.secrets["OPENROUTER_API_KEY"]
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
+OPENROUTER_API_KEY=os.getenv("OPENROUTER_API_KEY")
 embeddings = HuggingFaceEmbeddings(
     model_name="BAAI/bge-small-en-v1.5"
 )
@@ -34,27 +37,58 @@ llm = ChatOpenAI(
     model="deepseek/deepseek-chat-v3"
 )
 
-def ask_question(question):
-    retrieved_docs = retriever.invoke(question)
+contextualize_q_prompt=ChatPromptTemplate.from_messages(
+    [
+        (
+             "system",
+            """
+Given a chat history and latest user question,
+rewrite the question into a standalone question.
+
+Do NOT answer the question.
+
+Only rewrite it if needed.
+"""
+        ),MessagesPlaceholder("chat_history"),("human","{input}")
+    ]
+)
+history_aware_retriver=create_history_aware_retriever(llm,retriever,contextualize_q_prompt)
+
+qa_prompt=ChatPromptTemplate.from_messages([
+    ("system","""
+    you are simelab ai assistant ,use the context and history to answer.
+     Rules:
+     1. use only information from the context.
+     2.Do not use outside knowledge.
+     3.if answer is not in context,say :'i could not find that information on the simelabs website'
+     context:{context}
+     """),MessagesPlaceholder("chat_history"),("human","{input}")
+])
+
+def ask_question(question,chat_history):
+    print("Question",question)
+    retrieved_docs = history_aware_retriver.invoke(
+         {
+            "input": question,
+            "chat_history": chat_history
+        }
+    )
+    print("\nRETRIEVED DOCS")
+    for i, doc in enumerate(retrieved_docs):
+        print(f"\nChunk {i+1}")
+        print(doc.page_content[:500])
 
     context="\n".join([doc.page_content for doc in retrieved_docs])
-    sources=list(set(doc.metadata["source"]
+    sources=list(set(doc.metadata.get("source","Unknown")
                      for doc in retrieved_docs))
     
-    prompt=f"""
-    You are simelabs Ai assistant
-Rules:
-1. If the user is greeting you, thanking you, or engaging in casual conversation,
-   respond naturally and politely.
-
-2. If the user asks about SimeLabs, answer using ONLY the provided context.
-
-3. If the answer is not available in the context, say:
-   "I could not find that information on the SimeLabs website."
- Context:{context}
-Question:{question}
-"""
-    response=llm.invoke(prompt)
+    chain=qa_prompt|llm
+    response=chain.invoke({
+        "input":question,
+        "chat_history":chat_history,
+        "context":context
+    })
+    
     return response.content,sources
 
 
@@ -91,9 +125,16 @@ if question:
     })
     with st.chat_message("user"):
         st.markdown(question)
+    recent_messages=st.session_state.messages[-8:-1]
+    chat_history=[]
+    for msg in recent_messages:
+        if msg["role"]=="user":
+            chat_history.append(HumanMessage(content=msg["content"]))
+        else:
+            chat_history.append(AIMessage(content=msg["content"]))
     with st.chat_message("assistant"):
         with st.spinner("Searching simelabs knowledge base.."):
-            answer,sources=ask_question(question)
+            answer,sources=ask_question(question,chat_history)
             st.markdown(answer)
             # st.markdown(answer)
             st.markdown("##sources")
